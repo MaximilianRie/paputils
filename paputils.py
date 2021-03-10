7c4c1cb1d6df8c150b4335b07b1dc4addde29720
StealthyNiffler <45851669+StealthyNiffler@users.noreply.github.com>
2021-03-10 11:46:45 +0100

    added "col"s parameter in texify_table

--- paputils.py
+++ paputils.py
@@ -1,5 +1,6 @@
 import sympy
 import re
+
 def texify_expr(expr, *args):
     """expr should be a string with a valid python math expression and subs should
     be a list containing tuples with the name and the value of every variable that
@@ -16,8 +17,17 @@ def new_round(num, precision):
         return int(num)
     return round(num, precision)
 
-def texify_table(precision, *args):
-    """takes iterables as input and return latex table as string"""
+def texify_table(precision, cols, *args):
+    """takes iterables as input and return latex table as string
+        example: texify_table(precision, cols, *args)"""
+    if cols != 1:
+        n = max([len(elem) for elem in args])
+        rows = n//cols
+        arguments = []
+        for i in range(cols):
+            for elem in args:
+                arguments.append(list(elem[rows*i:rows*(i+1)]))
+        return texify_table(precision, 1, *arguments)
     table = ""
     for i in range(0, len(args[0])):
         for j, col in enumerate(args):
@@ -33,6 +43,9 @@ def texify_table(precision, *args):
     return table
 
 def get_stats(vals, precision=3):
+    """ return median and standard derrivative:
+    syntax: get_stats(vals, precision=3)
+    """
     n = len(vals)
     avr = new_round(np.sum(vals)/n, precision)
     sig = new_round(np.sqrt(1/(n-1) * np.sum((vals - avr)**2)), precision)
